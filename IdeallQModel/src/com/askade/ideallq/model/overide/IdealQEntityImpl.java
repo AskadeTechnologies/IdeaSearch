a8488665bb3eb22017da34758bad222a9550c412
adrian.ionita312@gmail.com <adrian.ionita312@gmail.com>
2016-07-31 01:46:42 +0300

    modificare get inserted id

--- IdeallQModel/src/com/askade/ideallq/model/overide/IdealQEntityImpl.java
+++ IdeallQModel/src/com/askade/ideallq/model/overide/IdealQEntityImpl.java
@@ -3,12 +3,17 @@ package com.askade.ideallq.model.overide;
 
 import java.math.BigDecimal;
 
+import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 import java.sql.SQLException;
 
+import java.sql.Types;
+
 import oracle.jbo.AttributeDef;
+import oracle.jbo.AttributeList;
+import oracle.jbo.server.DBTransaction;
 import oracle.jbo.server.EntityDefImpl;
 import oracle.jbo.server.EntityImpl;
 import oracle.jbo.server.TransactionEvent;
@@ -17,7 +22,7 @@ public class IdealQEntityImpl extends EntityImpl {
     public IdealQEntityImpl() {
         super();
     }
-
+/*
     @Override
     protected void doDML(int i, TransactionEvent transactionEvent) {
         //got to call first to super, so the record is posted
@@ -29,11 +34,12 @@ public class IdealQEntityImpl extends EntityImpl {
             populateAutoincrementAtt();
         }
     }
-
+*/
     /*
         * Determines if the Entity PK is marked as an autoincrement col
         * and executes a MySQL function to retrieve the last insert id
         */
+    /*
     private void populateAutoincrementAtt() {
         EntityDefImpl entdef = this.getEntityDef();
         AttributeDef pk = null;
@@ -56,6 +62,26 @@ public class IdealQEntityImpl extends EntityImpl {
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
+            }
+        }
+    }*/
+    
+    @Override
+    protected void create(AttributeList attributeList) {
+        super.create(attributeList);
+        for (AttributeDef def : getEntityDef().getAttributeDefs()) {
+            String sequenceName = (String) def.getProperty("SequenceName");
+            if (sequenceName != null) {
+                DBTransaction transaction = getDBTransaction();
+                String getSequenceString = "{call "+sequenceName+"(?)}";
+                CallableStatement callableStatement = transaction.createCallableStatement(getSequenceString, DBTransaction.DEFAULT);
+                try {
+                    callableStatement.registerOutParameter(1, Types.BIGINT);
+                    callableStatement.executeUpdate();
+                    setAttribute(def.getIndex(), callableStatement.getBigDecimal(1));
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
